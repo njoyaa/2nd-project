5741dbe21df52dce71b84c2d8ed92b6fdd952ca7
njoyaa <nemathulah.joyaa@tekscchool.us>
2022-06-22 11:19:00 -0700

    new update to the new branch

--- src/main/java/github/use/gitHubUpdating.java
+++ src/main/java/github/use/gitHubUpdating.java
@@ -3,7 +3,19 @@ package github.use;
 public class gitHubUpdating {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		
+		
+		
+		
 	}
-
+ public void sleep() {
+	 try {
+		Thread.sleep(1000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+ }
 	
 }
